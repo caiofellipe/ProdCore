33965364c218e7faf82699d1cbddb37fd08dfde9
Caio <caiofellipe80@gmail.com>
2023-06-19 19:55:35 -0300

    initial commit

---
+++ src/main/java/br/com/api/prodcore/ProdCoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.prodcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/prodcore/ProdCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.prodcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
