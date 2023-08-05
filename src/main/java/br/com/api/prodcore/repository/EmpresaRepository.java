3a484bac648dba64a4cbcf88477665fd9a6217bb
Caio <caiofellipe80@gmail.com>
2023-08-05 16:10:49 -0300

    bug innet dados duplicados

--- src/main/java/br/com/api/prodcore/repository/EmpresaRepository.java
+++ src/main/java/br/com/api/prodcore/repository/EmpresaRepository.java
@@ -14,7 +14,7 @@ public interface EmpresaRepository extends JpaRepository<Empresa, Long>{
 	
 	Empresa findByCnpj(String cnpj);
 	
-	@Query(value = "select * from empresa e"
+	@Query(value = "select distinct e.* from empresa e"
 			+ " inner join endereco en on en.empresa_id = e.id"
 			+ " inner join plano pl on pl.empresa_id  = e.id ", nativeQuery = true)
 	List<Empresa> findAllEmpresa();
